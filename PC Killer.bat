9a1e27a6e9e909518e0f945eaddb63074b2b3833
Yonatan Israeli <yonirisraeli@gmail.com>
2024-12-19 18:52:02 -0500

    Update PC Killer.bat

--- PC Killer.bat
+++ PC Killer.bat
@@ -2,10 +2,11 @@
 title PC Killer
 setlocal
 echo Program Name: PC Killer
-echo Version: 1.3.7
-echo Developer: @YonatanReuvenIsraeli
-echo Website: https://www.yonatanreuvenisraeli.dev
+echo Version: 1.3.8
 echo License: GNU General Public License v3.0
+echo Developer: @YonatanReuvenIsraeli
+echo GitHub: https://github.com/YonatanReuvenIsraeli
+echo Sponsor: https://github.com/sponsors/YonatanReuvenIsraeli 
 net session > nul 2>&1
 if not "%errorlevel%"=="0" goto "NotAdministrator"
 goto "Disclaimer"
